284a274b0e4323f6f89bf3b9e03dd34df956ed27
Kostas <Kwstaskor@hotmail.com>
2021-08-18 20:09:14 +0300

    game component category component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GameComponent } from './Components/game/game.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"Games", component:GameComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GameComponent } from './Components/game/game.component';
+import { CategoryComponent } from './Components/category/category.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent,
+    CategoryComponent
   ],
   imports: [
     BrowserModule,
